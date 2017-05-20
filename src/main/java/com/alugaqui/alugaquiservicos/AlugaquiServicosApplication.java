8e724fdee398c7b2b9e0e31067f82cb67e04ae13
Silvino Vieira <silvinov@gmail.com>
2017-05-20 20:09:28 -0300

    Cria projeto Spring Boot com Maven

---
+++ src/main/java/com/alugaqui/alugaquiservicos/AlugaquiServicosApplication.java
@@ -0,0 +1,12 @@
+package com.alugaqui.alugaquiservicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlugaquiServicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlugaquiServicosApplication.class, args);
+	}
+}
