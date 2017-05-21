a42ae40e72b93a321b1e9d5ce1c6014ae0d152fb
Silvino Vieira <silvinov@gmail.com>
2017-05-21 13:46:55 -0300

    Cria modelo de imóvel

---
+++ src/main/java/com/alugaqui/alugaquiservicos/model/Imovel.java
@@ -0,0 +1,34 @@
+package com.alugaqui.alugaquiservicos.model;
+
+/**
+ * 
+ * @author Silvino Vieira
+ *
+ */
+public class Imovel {
+
+  public String bairro;
+
+  public String cidade;
+
+  public String estado;
+
+  public String endereco;
+
+  public String cep;
+
+  public Double area;
+
+  public Integer quartos;
+
+  public Integer banheiros;
+
+  public Integer garagem;
+
+  public Double aluguel;
+
+  public Double condominio;
+
+  public String descricao;
+
+}
