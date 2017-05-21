ee276488dd5faad70bb142f90a0a422816c20e28
Silvino Vieira <silvinov@gmail.com>
2017-05-21 14:00:55 -0300

    Cria modelo de usuário

---
+++ src/main/java/com/alugaqui/alugaquiservicos/model/Usuario.java
@@ -0,0 +1,18 @@
+package com.alugaqui.alugaquiservicos.model;
+
+/**
+ * 
+ * @author Silvino Vieira
+ *
+ */
+public abstract class Usuario {
+
+  public String email;
+
+  public String nome;
+
+  public String sobrenome;
+
+  public String celular;
+
+}
