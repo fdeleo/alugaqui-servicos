b3b41200f376e3e4c5667aca57a86d7cddef1d42
Silvino Vieira <silvinov@gmail.com>
2017-05-30 14:36:56 -0300

    Cria usuários de teste em memória

--- src/main/java/com/alugaqui/alugaquiservicos/config/SecurityConfig.java
+++ src/main/java/com/alugaqui/alugaquiservicos/config/SecurityConfig.java
@@ -1,11 +1,20 @@
 package com.alugaqui.alugaquiservicos.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Autowired
+  public void configureAuth(AuthenticationManagerBuilder auth) throws Exception {
+    auth.inMemoryAuthentication().withUser("admin").password("admin").roles("CLIENTE", "CORRETOR")
+        .and().withUser("corretor").password("corretor").roles("CORRETOR").and().withUser("cliente")
+        .password("cliente").roles("CLIENTE");
+  }
 
 }
